d966bd158cb9fe3f718500c5efe4eda316d3a559
Raphael Lima Santana <raphaeltwins@hotmail.com>
2020-01-24 10:32:31 -0300

    Iteração 1 e 2 feitas

---
+++ Katas/Character.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Katas
+{
+    public class Character
+    {
+        private readonly int maxHealth = 1000;
+        private readonly int minHealth = 0;
+
+        public int Health { get; private set; }
+        public int Level { get; private set; }
+        public bool IsAlive { get; private set; }
+
+        public Character()
+        {
+            Health = maxHealth;
+            Level = 1;
+            IsAlive = true;
+        }
+
+        public void DealDamage(Character characterRecivier, int damage)
+        {
+            //Nada comentado sobre a possibilidade de atacar alguém morto
+            if (this.Equals(characterRecivier))
+                return;
+
+            damage = CalculateDamage(this, characterRecivier, damage);
+
+            characterRecivier.Health -= damage;
+            if (characterRecivier.Health <= minHealth)
+            {
+                characterRecivier.Health = minHealth;
+                characterRecivier.IsAlive = false;
+            }
+        }
+
+        private int CalculateDamage(Character characterAttacker, Character characterRecivier, int damage)
+        {
+            int differenceLevel = characterAttacker.Level - characterRecivier.Level;
+            if (differenceLevel >= 5)
+                damage += (100 * 50)/ damage;
+            if (differenceLevel <= -5)
+                damage -= (100 * 50) / damage;
+
+            return damage;
+        }
+
+        public void Heal(Character characterHealed, int Healing)
+        {
+            if (!characterHealed.IsAlive || !this.Equals(characterHealed))
+                return;
+
+            characterHealed.Health += Healing;
+
+            if (characterHealed.Health > maxHealth)
+                characterHealed.Health = maxHealth;
+        }
+
+        public void LevelUp(int level)
+        {
+            Level = level;
+        }
+    }
+}
---
+++ UnitTests/IterationOne.cs
@@ -0,0 +1,55 @@
+using FluentAssertions;
+using Katas;
+using Xunit;
+
+namespace UnitTests
+{
+    public class IterationOne
+    {
+        [Fact]
+        public void CreateCharacter_With__Health1000_Level1_IsAlive()
+        {
+            Character character = new Character();
+
+            character.Health.Should().Be(1000);
+            character.Level.Should().Be(1);
+            character.IsAlive.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Character_DealDamege_To_Characters()
+        {
+            Character characterAttacter = new Character();
+            Character characterRecivier = new Character();
+
+            characterAttacter.DealDamage(characterRecivier, 10000);
+
+            characterRecivier.Health.Should().Be(0);
+            characterRecivier.IsAlive.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Character_Can_Heal_A_Character()
+        {
+            Character characterHealer = new Character();
+            Character characterHealed = new Character();
+
+            characterHealer.DealDamage(characterHealed, 500);
+            characterHealer.Heal(characterHealed, 500);
+
+            characterHealed.Health.Should().Be(1000);
+        }
+
+        [Fact]
+        public void Character_Cant_Heal_Dead_Character()
+        {
+            Character characterHealer = new Character();
+            Character characterHealed = new Character();
+
+            characterHealer.DealDamage(characterHealed, 5000);
+            characterHealer.Heal(characterHealed, 500);
+
+            characterHealed.Health.Should().Be(1000);
+        }
+    }
+}
